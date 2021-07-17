39ad722924abfa9adf60c0261115488522063c81
Chiwan Ahn <chiwanan91@gmail.com>
2021-07-16 20:18:18 -0700

    feat: division and multiplication features done

--- go/money_test.go
+++ go/money_test.go
@@ -1,22 +1,51 @@
 package main
 
-import("testing")
+import (
+	"testing"
+)
 
-func TestMultiplication(t *testing.T) {
-	fiver := Dollar{
-		amount: 5,
-	}
-
-	tenner := fiver.Times(2)
-	if tenner.amount != 10 {
-		t.Errorf("Expected 10, got: [%d]", tenner.amount)
-	}
+type Money struct {
+	amount   float64
+	currency string
 }
 
-type Dollar struct {
-	amount int
+func (m Money) Times(multiplier int) Money {
+	return Money{amount: m.amount * float64(multiplier), currency: m.currency}
 }
 
-func (d Dollar) Times(multiplier int) Dollar {
-	return Dollar{amount: d.amount * multiplier}
+func (m Money) Divide(divisor int) Money {
+	return Money{amount: m.amount / float64(divisor), currency: m.currency}
+}
+
+func TestMultiplicationInDollars(t *testing.T) {
+	fiver := Money{
+		amount:   5,
+		currency: "USD",
+	}
+
+	actualResult := fiver.Times(2)
+	expectedResult := Money{amount: 10, currency: "USD"}
+	assertEqual(t, expectedResult, actualResult)
+}
+
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"}
+	actualResult := tenEuros.Times(2)
+	expectedResult := Money{amount: 20, currency: "EUR"}
+	assertEqual(t, expectedResult, actualResult)
+}
+
+// In Go, printing a struct with the %+v format “verb” prints the struct’s field names as well as values.
+
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}
+	actualResult := originalMoney.Divide(4)
+	expectedResult := Money{amount: 1000.5, currency: "KRW"}
+	assertEqual(t, expectedResult, actualResult)
+}
+
+func assertEqual(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("Expected [%+v], got: [%+v]", expected, actual)
+	}
 }
--- js/test_money.js
+++ js/test_money.js
@@ -1,15 +1,31 @@
 const assert = require("assert");
 
-class Dollar {
-  constructor(amount) {
+class Money {
+  constructor(amount, currency) {
     this.amount = amount;
+    this.currency = currency;
   }
 
   times(multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return new Money(this.amount * multiplier, this.currency);
+  }
+
+  divide(divisor) {
+    return new Money(this.amount / divisor, this.currency);
   }
 }
 
-fiver = new Dollar(5);
+fiver = new Money(5, "USD");
 tenner = fiver.times(2);
 assert.strictEqual(tenner.amount, 10);
+assert.strictEqual(tenner.currency, "USD");
+
+tenEuros = new Money(10, "EUR");
+twentyEuros = tenEuros.times(2);
+assert.strictEqual(twentyEuros.amount, 20);
+assert.strictEqual(twentyEuros.currency, "EUR");
+
+originalMoney = new Money(4002, "KRW");
+actualMoneyAfterDivision = originalMoney.divide(4);
+expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
